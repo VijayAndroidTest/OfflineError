6a812b3e6c0dbb8568e7126fcd15f8a40b685edd
Vijay <vijayarajan1003@gmail.com>
2026-05-22 14:16:17 +0530

    Build: Bump version parameters explicitly to 1.0.3 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.example.offlinetrack"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
